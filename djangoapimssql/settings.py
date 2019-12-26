a2f45dc4aa2e327bcc3a59096d143c54ab28812a
ossuchas <suchat.s14102526@gmail.com>
2019-12-26 12:50:01 +0700

    added

--- djangoapimssql/settings.py
+++ djangoapimssql/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'orgffa1kkg8=!u*f$+0)2lrcou(kc$(%-^p%&zyql-m@rrwdm9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
